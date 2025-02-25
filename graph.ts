300a2b555deae87cb91b126bffb926044ae632e8
Dexter Latcham <latchamd@lancaster.ac.uk>
2025-02-25 16:00:55 +0000

    refactored for global graph instance and separate title and legend blocks

--- graph.ts
+++ graph.ts
@@ -1,43 +1,15 @@
+enum graphType {
+    NONE =0,
+    BAR = 1,
+    PIE = 2
+}
+
 namespace displayGraph {
     let letters: { [key: string]: number[][] } = {
-        "k": [[1, 0, 0], [1, 0, 0], [1, 0, 1], [1, 1, 0], [1, 0, 1]],
-        "A": [[1, 1, 1], [1, 0, 1], [1, 1, 1], [1, 0, 1], [1, 0, 1]],
-        "B": [[1, 1, 0], [1, 0, 1], [1, 1, 0], [1, 0, 1], [1, 1, 0]],
-        "C": [[0, 1, 1], [1, 0, 0], [1, 0, 0], [1, 0, 0], [0, 1, 1]],
-        "D": [[1, 1, 0], [1, 0, 1], [1, 0, 1], [1, 0, 1], [1, 1, 0]],
-        "E": [[1, 1, 1], [1, 0, 0], [1, 1, 0], [1, 0, 0], [1, 1, 1]],
-        "F": [[1, 1, 1], [1, 0, 0], [1, 1, 0], [1, 0, 0], [1, 0, 0]],
-        "G": [[0, 1, 1], [1, 0, 0], [1, 0, 1], [1, 0, 1], [0, 1, 1]],
-        "H": [[1, 0, 1], [1, 0, 1], [1, 1, 1], [1, 0, 1], [1, 0, 1]],
-        "I": [[1, 1, 1], [0, 1, 0], [0, 1, 0], [0, 1, 0], [1, 1, 1]],
-        "J": [[1, 1, 1], [0, 1, 0], [0, 1, 0], [0, 1, 0], [1, 1, 0]],
-        "K": [[1, 0, 1], [1, 0, 1], [1, 1, 0], [1, 0, 1], [1, 0, 1]],
-        "L": [[1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 1, 1]],
-        "M": [[1, 0, 1], [1, 1, 1], [1, 0, 1], [1, 0, 1], [1, 0, 1]],
-        "N": [[1, 1, 0], [1, 0, 1], [1, 0, 1], [1, 0, 1], [1, 0, 1]],
-        "O": [[1, 1, 1], [1, 0, 1], [1, 0, 1], [1, 0, 1], [1, 1, 1]],
-        "P": [[1, 1, 1], [1, 0, 1], [1, 1, 1], [1, 0, 0], [1, 0, 0]],
-        "Q": [[0, 1, 0], [1, 0, 1], [1, 0, 1], [1, 0, 1], [0, 1, 1]],
-        "R": [[1, 1, 0], [1, 0, 1], [1, 1, 0], [1, 0, 1], [1, 0, 1]],
-        "S": [[0, 1, 1], [1, 0, 0], [0, 1, 0], [0, 0, 1], [1, 1, 0]],
-        "T": [[1, 1, 1], [0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0]],
-        "U": [[1, 0, 1], [1, 0, 1], [1, 0, 1], [1, 0, 1], [1, 1, 1]],
-        "V": [[1, 0, 1], [1, 0, 1], [1, 0, 1], [1, 0, 1], [0, 1, 0]],
-        "W": [[1, 0, 1], [1, 0, 1], [1, 0, 1], [1, 1, 1], [1, 0, 1]],
-        "X": [[1, 0, 1], [1, 0, 1], [0, 1, 0], [1, 0, 1], [1, 0, 1]],
-        "Y": [[1, 0, 1], [1, 0, 1], [0, 1, 0], [0, 1, 0], [0, 1, 0]],
-        "Z": [[1, 1, 1], [0, 0, 1], [0, 1, 0], [1, 0, 0], [1, 1, 1]],
-        "0": [[0, 1, 0], [1, 0, 1], [1, 0, 1], [1, 0, 1], [0, 1, 0]],
-        "1": [[0, 1, 0], [1, 1, 0], [0, 1, 0], [0, 1, 0], [1, 1, 1]],
-        "2": [[1, 1, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0], [1, 1, 1]],
-        "3": [[1, 1, 0], [0, 0, 1], [0, 1, 0], [0, 0, 1], [1, 1, 0]],
-        "4": [[1, 0, 1], [1, 0, 1], [1, 1, 1], [0, 0, 1], [0, 0, 1]],
-        "5": [[1, 1, 1], [1, 0, 0], [1, 1, 0], [0, 0, 1], [1, 1, 0]],
-        "6": [[0, 1, 1], [1, 0, 0], [1, 1, 0], [1, 0, 1], [0, 1, 0]],
-        "7": [[1, 1, 1], [0, 0, 1], [0, 1, 0], [0, 1, 0], [0, 1, 0]],
-        "8": [[0, 1, 0], [1, 0, 1], [0, 1, 0], [1, 0, 1], [0, 1, 0]],
-        "9": [[0, 1, 0], [1, 0, 1], [0, 1, 1], [0, 0, 1], [1, 1, 0]]
+        // curtesy of https://github.com/hgcummings/pixel-fonts/blob/master/data/slumbers.json
+        "A": [[1, 1, 1], [1, 0, 1], [1, 1, 1], [1, 0, 1], [1, 0, 1]], "B": [[1, 1, 0], [1, 0, 1], [1, 1, 0], [1, 0, 1], [1, 1, 0]], "C": [[0, 1, 1], [1, 0, 0], [1, 0, 0], [1, 0, 0], [0, 1, 1]], "D": [[1, 1, 0], [1, 0, 1], [1, 0, 1], [1, 0, 1], [1, 1, 0]], "E": [[1, 1, 1], [1, 0, 0], [1, 1, 0], [1, 0, 0], [1, 1, 1]], "F": [[1, 1, 1], [1, 0, 0], [1, 1, 0], [1, 0, 0], [1, 0, 0]], "G": [[0, 1, 1], [1, 0, 0], [1, 0, 1], [1, 0, 1], [0, 1, 1]], "H": [[1, 0, 1], [1, 0, 1], [1, 1, 1], [1, 0, 1], [1, 0, 1]], "I": [[1, 1, 1], [0, 1, 0], [0, 1, 0], [0, 1, 0], [1, 1, 1]], "J": [[1, 1, 1], [0, 1, 0], [0, 1, 0], [0, 1, 0], [1, 1, 0]], "K": [[1, 0, 1], [1, 0, 1], [1, 1, 0], [1, 0, 1], [1, 0, 1]], "L": [[1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 1, 1]], "M": [[1, 0, 1], [1, 1, 1], [1, 0, 1], [1, 0, 1], [1, 0, 1]], "N": [[1, 1, 0], [1, 0, 1], [1, 0, 1], [1, 0, 1], [1, 0, 1]], "O": [[1, 1, 1], [1, 0, 1], [1, 0, 1], [1, 0, 1], [1, 1, 1]], "P": [[1, 1, 1], [1, 0, 1], [1, 1, 1], [1, 0, 0], [1, 0, 0]], "Q": [[0, 1, 0], [1, 0, 1], [1, 0, 1], [1, 0, 1], [0, 1, 1]], "R": [[1, 1, 0], [1, 0, 1], [1, 1, 0], [1, 0, 1], [1, 0, 1]], "S": [[0, 1, 1], [1, 0, 0], [0, 1, 0], [0, 0, 1], [1, 1, 0]], "T": [[1, 1, 1], [0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0]], "U": [[1, 0, 1], [1, 0, 1], [1, 0, 1], [1, 0, 1], [1, 1, 1]], "V": [[1, 0, 1], [1, 0, 1], [1, 0, 1], [1, 0, 1], [0, 1, 0]], "W": [[1, 0, 1], [1, 0, 1], [1, 0, 1], [1, 1, 1], [1, 0, 1]], "X": [[1, 0, 1], [1, 0, 1], [0, 1, 0], [1, 0, 1], [1, 0, 1]], "Y": [[1, 0, 1], [1, 0, 1], [0, 1, 0], [0, 1, 0], [0, 1, 0]], "Z": [[1, 1, 1], [0, 0, 1], [0, 1, 0], [1, 0, 0], [1, 1, 1]], "0": [[0, 1, 0], [1, 0, 1], [1, 0, 1], [1, 0, 1], [0, 1, 0]], "1": [[0, 1, 0], [1, 1, 0], [0, 1, 0], [0, 1, 0], [1, 1, 1]], "2": [[1, 1, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0], [1, 1, 1]], "3": [[1, 1, 0], [0, 0, 1], [0, 1, 0], [0, 0, 1], [1, 1, 0]], "4": [[1, 0, 1], [1, 0, 1], [1, 1, 1], [0, 0, 1], [0, 0, 1]], "5": [[1, 1, 1], [1, 0, 0], [1, 1, 0], [0, 0, 1], [1, 1, 0]], "6": [[0, 1, 1], [1, 0, 0], [1, 1, 0], [1, 0, 1], [0, 1, 0]], "7": [[1, 1, 1], [0, 0, 1], [0, 1, 0], [0, 1, 0], [0, 1, 0]], "8": [[0, 1, 0], [1, 0, 1], [0, 1, 0], [1, 0, 1], [0, 1, 0]], "9": [[0, 1, 0], [1, 0, 1], [0, 1, 1], [0, 0, 1], [1, 1, 0]]
     }
+
     function write(word: string, startx: number, starty: number) {
         word.split('').forEach((letter, count) => {
             const upperLetter = letter.toUpperCase();
@@ -67,120 +39,67 @@ namespace displayGraph {
         }
     }
 
-    //% blockId=graphcolorindexpicker block="$index" blockHidden=true
-    //% index.fieldEditor="colornumber"
-    //% index.fieldOptions.valueMode="index"
-    //% index.fieldOptions.colours='["#000000","#ffffff","#ff2121","#ff93c4","#ff8135","#fff609","#249ca3","#78dc52","#003fad","#87f2ff","#8e2ec4","#a4839f","#5c406c","#e5cdc4","#91463d","#000000"]'
-    //% index.fieldOptions.decompileLiterals="true"
-    export function __colorIndexPicker(index: number) {
-        return index;
-    }
+    let SCREEN_WIDTH = 0;
+    let SCREEN_HEIGHT = 0;
+    let BASE_BORDER = 0;
+    let TOP_BORDER = 0;
+    let LEFT_BORDER = 0;
+    let data:inputData[]|null=null;
+    let TITLE = "";
+    let SHOWKEY = false;
+    let SELECTEDGRAPH =  graphType.NONE;
 
-
-    //% block="name $name value $value colour $c=graphcolorindexpicker"
-    //% value.shadow=math_number
-    //% blockId=graphCreateInput
-    //% c.defl=8
-    //% weight=80
-    export function createIV(name: string, value: number, c: number): inputData {
-        return new inputData(name, value, c);
-    }
-
-
-    function drawKey(data: inputData[]): void {
-        const SCREEN_WIDTH = bitmaps.width(screen());
-        const SCREEN_HEIGHT = bitmaps.height(screen());
-
-        const KEY_Y_START = 5;
-        const KEY_X_START = 2;
-
-        for (let i = 0; i < data.length; i++) {
-            let akey = data[i].name;
-            for (let x = KEY_X_START; x < KEY_X_START + 4; x++) {
-                for (let y = KEY_Y_START; y < KEY_Y_START + 5; y++) {
-                    screen().setPixel(x, y + (i * 10), data[i].colour);
-                }
+    export function redraw(){
+        screen().fill(1);
+        SCREEN_WIDTH = bitmaps.width(screen());
+        SCREEN_HEIGHT = bitmaps.height(screen());
+        TOP_BORDER = 5;
+        LEFT_BORDER=5;
+        BASE_BORDER = 0;
+        if (TITLE!="") {
+            let pixelsNeeded = TITLE.length * 4;
+            let startX = (SCREEN_WIDTH - pixelsNeeded) / 2;
+            if (startX <= 10) {
+                //title too long
+            } else {
+                write(TITLE, startX, TOP_BORDER);
+                TOP_BORDER += 7;
             }
-            write(akey, KEY_X_START + 10, KEY_Y_START + i * 10);
         }
-    }
-    //% block="plot pie chart array $data"
-    //% blockId=graphPieChartArray
-    //% data.shadow=lists_create_with
-    //% data.defl=graphCreateInput
-    //% weight=100
-    export function pieChartImpl(data: inputData[],includeKey=false): void {
-        screen().fill(1); // fill with white
-        const SCREEN_WIDTH = bitmaps.width(screen());
-        const SCREEN_HEIGHT = bitmaps.height(screen());
-
-        if(includeKey){
-            drawKey(data);
+        if(!data){
+            return;
         }
-
-        let values: number[] = [];
-        for (let i = 0; i < data.length; i++) {
-            values.push(data[i].value);
-        }
-
-        const valueSum = values.reduce((a, b) => a + b, 0);
-        const normalisedValues = values.map(val => val / valueSum);
-
-        const cumulativeValues = [];
-        let cumulativeSum = 0;
-        for (const val of normalisedValues) {
-            cumulativeValues.push(cumulativeSum + val);
-            cumulativeSum += val;
-        }
-
-        // valsScaled is between 0 and 1
-
-        const radius = (SCREEN_HEIGHT / 2) * 0.8;
-        const yMid = Math.floor(SCREEN_HEIGHT / 2);
-
-        let xMid = Math.floor(SCREEN_WIDTH/2);
-        
-        if(includeKey){
-            drawKey(data);
-            xMid = SCREEN_WIDTH - (radius + 5);
-        }
-
-        for (let x = -radius; x <= radius; x++) {
-            for (let y = -radius; y <= radius; y++) {
-                if (x * x + y * y <= radius * radius) {
-                    // Between -pi and pi
-                    const angle = Math.atan2(y, x);
-                    const angleLerp = (angle + Math.PI) / (2 * Math.PI);
-
-                    for (let i = 0; i < cumulativeValues.length; i++) {
-                        if (angleLerp <= cumulativeValues[i]) {
-                            screen().setPixel(xMid + x, yMid + y, data[i].colour);
-                            break;
-                        }
+        if (SHOWKEY) {
+            let maxLen=0;
+            for (let i = 0; i < data.length; i++) {
+                let akey = data[i].name;
+                for (let x = LEFT_BORDER; x < LEFT_BORDER + 4; x++) {
+                    for (let y = TOP_BORDER; y < TOP_BORDER + 5; y++) {
+                        screen().setPixel(x, y + (i * 10), data[i].colour);
                     }
                 }
+                if(data[i].name.length > maxLen){
+                    maxLen=data[i].name.length;
+                }
+                write(akey, LEFT_BORDER + 10, TOP_BORDER + i * 10);
             }
+            TOP_BORDER += (10*data.length);
+            LEFT_BORDER += (10 + (maxLen*4));
+        }
+        switch(SELECTEDGRAPH){
+            case graphType.NONE:
+                return;
+            case graphType.BAR:
+                drawBarChart();
+                return;
+            case graphType.PIE:
+                drawPieChart();
+                return;
         }
     }
 
-    //% block="plot bar chart for array $data"
-    //% blockId=graphBarChartArray
-    //% data.shadow=lists_create_with
-    //% data.defl=graphCreateInput
-    //% weight=100
-    export function barChart(data: inputData[], includeKey: boolean = false) {
-        screen().fill(1); // fill with white
-        const SCREEN_WIDTH = bitmaps.width(screen());
-        const SCREEN_HEIGHT = bitmaps.height(screen());
-        const BASE_BORDER = 10;
-        let TOP_BORDER = 10;
-        const LEFT_BORDER = 10;
-
-        if (includeKey) {
-            drawKey(data);
-            TOP_BORDER = (7 * data.length) + 10;
-        }
-
+    function drawBarChart(): void {
+        BASE_BORDER =10;
 
         let minValue = data[0].value;
         let maxValue = data[0].value;
@@ -197,8 +116,6 @@ namespace displayGraph {
         }
         const numBars = values.length;
 
-
-
         let xAxisY = 0;
         if (minValue >= 0) {
             xAxisY = SCREEN_HEIGHT - (BASE_BORDER - 1 + TOP_BORDER);
@@ -209,11 +126,11 @@ namespace displayGraph {
             screen().setPixel(i, xAxisY + TOP_BORDER, 0);
         }
 
-        for (let i = 0; i < SCREEN_HEIGHT; i++) {
-            screen().setPixel(LEFT_BORDER - 1, i, 0);
+        for (let i = TOP_BORDER; i < SCREEN_HEIGHT; i++) {
+            screen().setPixel(4, i, 0);
         }
 
-        const BAR_SCREEN_WIDTH = SCREEN_WIDTH - LEFT_BORDER;
+        const BAR_SCREEN_WIDTH = SCREEN_WIDTH - 5;
         const BAR_SCREEN_HEIGHT = SCREEN_HEIGHT - BASE_BORDER - TOP_BORDER;
         if (numBars >= BAR_SCREEN_WIDTH) {
             //error, too much to plot
@@ -232,7 +149,7 @@ namespace displayGraph {
 
         //if there are no negative values
 
-        let xOffset = LEFT_BORDER + BAR_BLOCK_SEPARATION;
+        let xOffset = 5 + BAR_BLOCK_SEPARATION;
         if (minValue >= 0) {
             for (let i = 0; i < values.length; i++) {
                 let heightLerp = values[i] / (maxValue);
@@ -258,6 +175,104 @@ namespace displayGraph {
                 xOffset += (BAR_BLOCK_WIDTH + BAR_BLOCK_SEPARATION);
             }
         }
+    }
+
+    function drawPieChart(): void {
+        let values: number[] = [];
+        for (let i = 0; i < data.length; i++) {
+            values.push(data[i].value);
+        }
+
+        const valueSum = values.reduce((a, b) => a + b, 0);
+        const normalisedValues = values.map(val => val / valueSum);
+
+        const cumulativeValues = [];
+        let cumulativeSum = 0;
+        for (const val of normalisedValues) {
+            cumulativeValues.push(cumulativeSum + val);
+            cumulativeSum += val;
+        }
+
+        // valsScaled is between 0 and 1
+
+        let yRange = SCREEN_HEIGHT*0.8;
+        let xRange = SCREEN_WIDTH-LEFT_BORDER;
+        const radius = (Math.min(xRange,yRange) /2)*0.9;
+        const yMid = Math.floor(SCREEN_HEIGHT / 2);
+
+        let xMid = Math.floor((SCREEN_WIDTH-LEFT_BORDER)/2)+LEFT_BORDER;
+        
+        for (let x = -radius; x <= radius; x++) {
+            for (let y = -radius; y <= radius; y++) {
+                if (x * x + y * y <= radius * radius) {
+                    // Between -pi and pi
+                    const angle = Math.atan2(y, x);
+                    const angleLerp = (angle + Math.PI) / (2 * Math.PI);
+
+                    for (let i = 0; i < cumulativeValues.length; i++) {
+                        if (angleLerp <= cumulativeValues[i]) {
+                            screen().setPixel(xMid + x, yMid + y, data[i].colour);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    //
+    //% blockId=graphcolorindexpicker block="$index" blockHidden=true
+    //% index.fieldEditor="colornumber"
+    //% index.fieldOptions.valueMode="index"
+    //% index.fieldOptions.colours='["#000000","#ffffff","#ff2121","#ff93c4","#ff8135","#fff609","#249ca3","#78dc52","#003fad","#87f2ff","#8e2ec4","#a4839f","#5c406c","#e5cdc4","#91463d","#000000"]'
+    //% index.fieldOptions.decompileLiterals="true"
+    export function __colorIndexPicker(index: number) {
+        return index;
+    }
+
+
+    //% block="name $name value $value colour $c=graphcolorindexpicker"
+    //% value.shadow=math_number
+    //% blockId=graphCreateInput
+    //% c.defl=8
+    //% weight=80
+    export function createIV(name: string, value: number, c: number): inputData {
+        return new inputData(name, value, c);
+    }
+
+    //% block="plot bar chart for array $providedData"
+    //% blockId=graphBarChartArray
+    //% providedData.shadow=lists_create_with
+    //% providedData.defl=graphCreateInput
+    //% weight=100
+    export function barChart(providedData: inputData[]) {
+        SELECTEDGRAPH=graphType.BAR;
+        data = providedData;
+        redraw()
+    }
+
+    //% block="plot pie chart for array $providedData"
+    //% blockId=graphPieChartArray
+    //% providedData.shadow=lists_create_with
+    //% providedData.defl=graphCreateInput
+    //% weight=100
+    export function pieChart(providedData: inputData[]) {
+        SELECTEDGRAPH=graphType.PIE;
+        data = providedData;
+        redraw()
+    }
+    
+    //% block="set title $providedTitle"
+    export function setTitle(providedTitle:string):void{
+        TITLE=providedTitle;
+        redraw();
+    }
+    
+    //%block="draw key $keyChoice"
+    //keyChoice.shadow=toggleOnOff
+    //keyChoice.defl=false
+    export function drawKey(keyChoice:boolean):void{
+        SHOWKEY=keyChoice;
+        redraw();
     }
 }
 
